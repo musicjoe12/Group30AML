665e1d384729526a7ec7b9a804e05dfd4aaa1b1f
musicjoe12 <116874047+musicjoe12@users.noreply.github.com>
2024-11-14 13:49:12 +0000

    Navbar + Assets folder + CSS folder

--- libary-management-system/src/Components/Navbar.js
+++ libary-management-system/src/Components/Navbar.js
@@ -1,31 +1,90 @@
 import React from 'react';
-import { Anchor } from 'antd';
-import { Link } from 'react-router-dom'; // Import the Link component from react-router-dom
+import { Anchor, Layout } from 'antd';
+import '../CSS/navbar.css';
+
+//Assets
+import logo from '../Assets/logo.png';
+
+const { Header } = Layout;
 
 function Navbar() {
   return (
-    <>
-      <div style={{ padding: '20px' }}>
-        <Anchor direction="horizontal">
-          <Anchor.Link key="home" href="/" title="Home" />
+    <Layout>
+      <Header className="navbar-header">
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center', 
+          justifyContent: 'flex-start',
+          width: '100%',
+        }}
+      >
+        <img
+          src={logo}
+          alt="Logo"
+          style={{
+            height: '70px',
+            marginRight: '20px', 
+          }}
+        />
+        <span
+          style={{
+            color: 'white',
+            fontSize: '24px',
+            fontWeight: 'bold',
+          }}
+        >
+          Library Books
+        </span>
+      </div>
+
+        <Anchor
+          direction="horizontal"
+          className="navbar-container"
+        >
+          <Anchor.Link
+            key="home"
+            href="/"
+            title="Home"
+            className="navbar-link" 
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/';
+            }}
+          />
           <Anchor.Link
             key="browse-media"
             href="/browse-media"
             title="Browse Media"
+            className="navbar-link"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/browse-media';
+            }}
           />
           <Anchor.Link
             key="manage-media"
             href="/manage-media"
             title="Manage Media"
+            className="navbar-link" 
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/manage-media';
+            }}
           />
           <Anchor.Link
             key="manage-inventory"
             href="/manage-inventory"
             title="Manage Inventory"
+            className="navbar-link"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/manage-inventory';
+            }}
           />
         </Anchor>
-      </div>
-    </>
+      </Header>
+    </Layout>
   );
 }
 
