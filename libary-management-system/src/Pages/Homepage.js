6998835002ce3e13e7d24b8c1df3a5b9f8caa08a
musicjoe12 <116874047+musicjoe12@users.noreply.github.com>
2024-11-15 17:58:10 +0000

    Home page

--- libary-management-system/src/Pages/Homepage.js
+++ libary-management-system/src/Pages/Homepage.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Carousel, Button } from 'antd';
+import { Carousel, Button, Flex, Splitter, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
+
+//Assets
+import logo1 from '../Assets/logo1.png';
+import logo2 from '../Assets/logo2.png';
+import logo3 from '../Assets/logo3.png';
+
 
 const blueBoxStyle = {
   height: '50vh',
@@ -18,17 +24,31 @@ const blueBoxStyle = {
 };
 
 const carouselContainerStyle = {
-  height: '50vh',
+  height: '40vh',
 };
 
 const contentStyle = {
   margin: 0,
-  height: '350px',
+  height: '370px',
   color: '#fff',
   lineHeight: '160px',
   textAlign: 'center',
   background: '#364d79',
 };
+
+const Desc = ({ title, content, imageUrl }) => (
+<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <Typography.Title level={5}>
+      {title}
+    </Typography.Title>
+    <p>{content}</p>
+    <img 
+      src={imageUrl} 
+      alt="Image" 
+      style={{ width: '35%', height: 'auto', borderRadius: '8px', margin: '20px 0' }} 
+    />
+  </div>
+);
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -36,11 +56,13 @@ const Homepage = () => {
   const handleGetStarted = () => navigate('/browse-media');
 
   return (
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <div style={{display: 'flex', flexDirection: 'column' }}>
       <div style={blueBoxStyle}>
-        <p>Welcome to the library booking system!</p>
+        <p>Discover a world of knowledge with the Advanced Media Library (AML), offering books, journals, multimedia, and more across England's largest library network. Whether you're in Sheffield, Manchester, or a smaller town, AML provides seamless access to resources through our in-branch services, web-mobile app, and 24/7 online systems.</p>
         <Button type="primary" onClick={handleGetStarted}>Get Started!</Button>
       </div>
+
+      {/* Carousel Container */}
       <div style={carouselContainerStyle}>
         <Carousel arrows infinite={false}>
           <div>
@@ -57,6 +79,43 @@ const Homepage = () => {
           </div>
         </Carousel>
       </div>
+
+     
+      <div style={{ textAlign: 'center', marginTop: '30px' }}>
+        <Typography.Title level={2}>Who Do We Work For?</Typography.Title>
+      </div>
+
+      
+      <Splitter
+        style={{
+          height: 400,
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          marginTop: '60px', 
+        }}
+       
+      >
+        <Splitter.Panel collapsible>
+        <Desc 
+            title="Students" 
+            content="Borrow our books in order to help your studies." 
+            imageUrl={logo3}
+          />
+        </Splitter.Panel>
+        <Splitter.Panel collapsible={{ start: false }}>
+        <Desc 
+            title="Teachers" 
+            content="Find resources to help students explore your subject at a deeper level." 
+            imageUrl={logo2}
+          />
+        </Splitter.Panel>
+        <Splitter.Panel>
+        <Desc 
+            title="Libarians" 
+            content="Visit us to borrow books of your liking." 
+            imageUrl={logo1}
+          />
+        </Splitter.Panel>
+      </Splitter>
     </div>
   );
 };
