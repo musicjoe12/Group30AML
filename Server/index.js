a5eea7807a625e789bd2480dca86ac684e07af4c
Samuel12372 <c2028335@hallam.shu.ac.uk>
2024-11-15 14:31:03 +0000

    fix

--- Server/index.js
+++ Server/index.js
@@ -3,17 +3,25 @@
 //ctrl + c to stop server
 //console.log(app); //shows methods and properties of express app
 
+require("dotenv").config(); 
 const express = require('express');
-const app = express(); // Create an express app
-const cors = require('cors'); // Cross Origin Resource Sharing
+const mongoose = require('mongoose');
+const app = express();
 
-app.use(cors()); // Use cors middleware
+const PORT = 8080;  
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(8080, () => {   
-    console.log('Server is listening on port 8080');
+mongoose.connect("mongodb+srv://samuel:lqBvVKDKsE6MbvOZ@backend.psehq.mongodb.net/AML?retryWrites=true&w=majority&appName=backend")
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err);
 });
