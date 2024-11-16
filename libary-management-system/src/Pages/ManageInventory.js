98bafa7bd2f504d1c6b5a9997fdaad1280d6efba
musicjoe12 <116874047+musicjoe12@users.noreply.github.com>
2024-11-16 18:25:29 +0000

    Started manage inventory

--- libary-management-system/src/Pages/ManageInventory.js
+++ libary-management-system/src/Pages/ManageInventory.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { Space, Table, Tag } from 'antd';
 
-function ManageInventory() {
-  return <h1>Manage Inventory Page</h1>;
-}
+const columns = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    key: 'title',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Author',
+    dataIndex: 'author',
+    key: 'author',
+  },
+  {
+    title: 'Genre',
+    dataIndex: 'genre',
+    key: 'genre',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>View Details</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    genre: 'Classic',
+    tags: ['romance', 'tragedy'],
+  },
+  {
+    key: '2',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Fiction',
+    tags: ['coming-of-age', 'racism'],
+  },
+  {
+    key: '3',
+    title: '1984',
+    author: 'George Orwell',
+    genre: 'Dystopian',
+    tags: ['political', 'surveillance'],
+  },
+];
+
+const ManageInventory = () => <Table columns={columns} dataSource={data} />;
 
 export default ManageInventory;
