763951e82e4ed32103e370f791ca620e1e393aa4
shubz0 <sohaib.khan.khan11@gmail.com>
2024-11-17 20:21:40 +0000

    Added Hamburger Menu
    Added hamburger menu with placeholder links same as the ones already in the navbar

--- libary-management-system/src/Components/Navbar.js
+++ libary-management-system/src/Components/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Anchor, Layout, Input } from 'antd';
+import React, { useState } from 'react';
+import { Anchor, Layout, Input, Drawer, Button, Menu } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 import '../CSS/navbar.css';
 
 //Assets
 import logo from '../Assets/logo.png';
+import AnchorLink from 'antd/es/anchor/AnchorLink';
 
 const { Header } = Layout;
 const { Search } = Input;
@@ -16,6 +18,16 @@ function Navbar() {
     console.log("Search Input:", value);
 
     //search logic to be added here
+  };
+
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+
+  const showDrawer = () => {
+    setIsDrawerVisible(true);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerVisible(false);
   };
 
   return (
@@ -59,9 +71,63 @@ function Navbar() {
           marginLeft: '50px',
         }}
         />
-
-
         </div>
+
+        {/* Hamburger Menu Btn */}
+
+        <Button
+        type="text"
+        icon={<MenuOutlined style={{ fontSize: '24px', color: 'white' }} />}
+        onClick={showDrawer}
+        style={{ marginLeft: 'auto '}}
+        />
+
+        {/* Drawer Component */}
+
+        <Drawer
+        title='Menu'
+        placement='right'
+        onClose={closeDrawer}
+        visible={isDrawerVisible}>
+          <Anchor direction='vertical' className='navbar-container'>
+            <Anchor.Link
+            key="home"
+            href='/homepage'
+            title="Home"
+            className='navbar-link'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/homepage';
+            }}/>
+            <Anchor.Link
+            key="browse-media"
+            href='/browse-media'
+            title="Browse Media"
+            className='navbar-link'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/browse-media';
+            }}/>
+            <Anchor.Link
+            key="manage-media"
+            href='/manage-media'
+            title="Manage Media"
+            className='navbar-link'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/manage-media';
+            }}/>
+            <Anchor.Link
+            key="manage-inventory"
+            href='/manage-inventory'
+            title="Manage Inventory"
+            className='navbar-link'
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = '/manage-inventory';
+            }}/>
+          </Anchor>
+        </Drawer>
 
         <Anchor
           direction="horizontal"
