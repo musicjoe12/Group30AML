1021788d19ef520772f7c5aee81922b3573f2bb4
Samuel12372 <c2028335@hallam.shu.ac.uk>
2024-11-25 15:57:51 +0000

    page loader start

---
+++ libary-management-system/src/Components/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+import TailSpin from 'react-loader-spinner';
+
+function Loader() {
+        
+    return (
+        <div className="loader-container">
+
+            
+        
+
+        </div>
+    );
+
+}
+
+    
+
+export default Loader;
