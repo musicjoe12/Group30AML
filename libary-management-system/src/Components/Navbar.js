f033ad43de633f7b9e8c0568ffa6a5d91bb23e50
musicjoe12 <116874047+musicjoe12@users.noreply.github.com>
2024-11-27 17:12:48 +0000

    added login cookies + css fixes

--- libary-management-system/src/Components/Navbar.js
+++ libary-management-system/src/Components/Navbar.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Layout, Input, Menu, Button, Switch, Modal, Form, List, Avatar, Input as AntInput } from 'antd';
+import { Layout, Input, Menu, Button, Switch, Modal, List } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/navbar.css'; // Custom CSS file
-import { UserContext } from '../Context/UserContext';
+import { UserContext } from '../Context/UserContext';  // Import UserContext
 import { useSearch } from '../Context/SearchContext'; 
-
+import Cookies from 'js-cookie'; // Import js-cookie
+import { SettingOutlined } from '@ant-design/icons'; // Import settings cog icon
 
 
 // Assets
@@ -15,13 +16,12 @@ const { Header } = Layout;
 const { Search } = Input;
 
 function Navbar() {
-
-  const { setUserId } = useContext(UserContext);
+  const { setUserId, setUserName, userName } = useContext(UserContext);  // Destructure userName and setUserName
   const [isToggled, setIsToggled] = useState(false);
   const [users, setUsers] = useState([]); 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [loginText, setLoginText] = useState("Login");
-
+  const [isSettingsVisible, setIsSettingsVisible] = useState(false); // For settings modal visibility
 
 
   // Initialize toggle state from localStorage
@@ -30,14 +30,23 @@ function Navbar() {
     if (savedToggleState !== null) {
       setIsToggled(savedToggleState === 'true');
     }
-  }, []);
+
+    // Check cookies to set the logged-in user on page load
+    const storedUserName = Cookies.get('userName');
+    const storedUserId = Cookies.get('userId');
+
+    if (storedUserName && storedUserId) {
+      // Set the user details from cookies to context and update login button text
+      setUserName(storedUserName);
+      setUserId(storedUserId);
+      setLoginText(storedUserName);
+    }
+  }, [setUserName, setUserId]); // Only update when the component mounts
 
   const handleToggle = (checked) => {
     setIsToggled(checked);
     localStorage.setItem('sliderState', checked); // Save state to localStorage
   };
-
-
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -47,42 +56,71 @@ function Navbar() {
     setIsModalVisible(false);
   };
 
-  const fetchUsers = async() => {
+  const fetchUsers = async () => {
     await axios.get("http://localhost:8080/api/users")
-    .then(res => {
-      setUsers(res.data); 
-      //setIsModalVisible(false);
-    })
-    .catch(err => console.log(err));
+      .then(res => {
+        setUsers(res.data); 
+      })
+      .catch(err => console.log(err));
   };
 
-  const handleLogin = async(userId) => {
+  const handleLogin = async (userId, userName) => {
     console.log("User ID:", userId);
+
+    // Store user details in cookies for persistence across pages
+    Cookies.set('userId', userId, { expires: 7 });
+    Cookies.set('userName', userName, { expires: 7 });
+
+    // Update the context with the userId and userName
     setUserId(userId);
-    setIsModalVisible(false);
-    setLoginText(`${users.find(user => user._id === userId).first_name}`);    
+    setUserName(userName);
+
+    // Update the login button text
+    setLoginText(userName);
   };
-  
-  //Navbar search 
+
+  const handleLogout = () => {
+    // Remove cookies on logout
+    Cookies.remove('userId');
+    Cookies.remove('userName');
+
+    // Reset context values to logged-out state
+    setUserId(null);
+    setUserName(null);
+
+    // Reset login button text
+    setLoginText("Login");
+    setIsSettingsVisible(false); 
+  };
+
+  const showSettingsModal = () => {
+    setIsSettingsVisible(true);
+  };
+
+  const handleCancelSettings = () => {
+    setIsSettingsVisible(false);
+  };
+
+  // Navbar search 
   const { searchValue, setSearchValue } = useSearch(); // Get searchValue and setSearchValue
   const navigate = useNavigate();
 
   const onSearch = (value) => {
     navigate(`/browse-media?search=${value}`);
   };
-  
+
   return (
     <Layout>
       {/* Top Navbar */}
       <Header className="navbar-header">
         <div className="navbar-top">
           {/* Left: Logo and Title */}
-          <div class="navbar-left">
-          <a href="Homepage" class="navbar-home-link">
-            <img src={logo} alt="AML Logo" class="navbar-logo" />
-            <span class="navbar-title">AML</span>
-          </a>
-        </div>
+          <div className="navbar-left">
+            <a href="Homepage" className="navbar-home-link">
+              <img src={logo} alt="AML Logo" className="navbar-logo" />
+              <span className="navbar-title">AML</span>
+            </a>
+          </div>
 
           {/* Right: Search Bar and Login Button */}
           <div className="navbar-right">
@@ -93,69 +131,89 @@ function Navbar() {
               value={searchValue} // Controlled input
               onChange={(e) => setSearchValue(e.target.value)} // Update search value
               onSearch={onSearch}
-
             />
             <Button type="primary" className="navbar-login" onClick={() => {
               showModal();
               fetchUsers();
-              }}>
-              {loginText}
+            }}>
+              {loginText} {/* Display the user's name if logged in */}
             </Button>
+          {/* Settings Icon next to Login Button */}
+          <SettingOutlined
+            className="settings-icon"
+            style={{ fontSize: '24px', color: '#white', marginLeft: '10px', cursor: 'pointer' }}
+            onClick={showSettingsModal} // Open the settings modal
+          />      
           </div>
         </div>
       </Header>
 
       {/* Secondary Navbar */}
       <div className="secondary-navbar">
-  <div style={{ flexGrow: 1 }}>
-    <Menu mode="horizontal" className="secondary-menu">
-      <Menu.Item key="browse-media">
-        <a href="/browse-media">Browse Media</a>
-      </Menu.Item>
-      <Menu.Item key="manage-media">
-        <a href="/manage-media">Manage Media</a>
-      </Menu.Item>
-      {isToggled && (
-        <Menu.Item key="manage-inventory">
-          <a href="/manage-inventory">Manage Inventory</a>
-        </Menu.Item>
-      )}
-    </Menu>
-  </div>
-  <div className="toggle-container">
-    <Switch
-      checked={isToggled}
-      onChange={handleToggle}
-      checkedChildren="On"
-      unCheckedChildren="Off"
-    />
-    <span className="toggle-description">
-      {isToggled
-        ? 'Branch Manager Mode Enabled'
-        : 'Branch Manager Mode Disabled'}
-    </span>
-  </div>
-</div>
+        <div style={{ flexGrow: 1 }}>
+          <Menu mode="horizontal" className="secondary-menu">
+            <Menu.Item key="browse-media">
+              <a href="/browse-media">Browse Media</a>
+            </Menu.Item>
+            <Menu.Item key="manage-media">
+              <a href="/manage-media">Manage Media</a>
+            </Menu.Item>
+            {isToggled && (
+              <Menu.Item key="manage-inventory">
+                <a href="/manage-inventory">Manage Inventory</a>
+              </Menu.Item>
+            )}
+          </Menu>
+        </div>
+        <div className="toggle-container">
+          <Switch
+            checked={isToggled}
+            onChange={handleToggle}
+            checkedChildren="On"
+            unCheckedChildren="Off"
+          />
+          <span className="toggle-description">
+            {isToggled
+              ? 'Branch Manager Mode Enabled'
+              : 'Branch Manager Mode Disabled'}
+          </span>
+        </div>
+      </div>
 
-{/* Login Modal */}
-<Modal
-        title="Select User to Login"
-        visible={isModalVisible}
-        onCancel={handleCancel}
+      /* Login Modal */
+        <Modal
+          title="Select User to Login"
+          visible={isModalVisible}
+          onCancel={handleCancel}
+          footer={null}
+        >
+          <List
+            itemLayout="horizontal"
+            dataSource={users}
+            renderItem={(user) => (
+          <List.Item onClick={() => {
+            handleLogin(user._id, `${user.first_name} ${user.last_name}`);
+            setIsModalVisible(false); 
+          }} className="user-item">
+            <List.Item.Meta
+              title={`${user.first_name} ${user.last_name}`}
+              description={user.email}
+            />
+          </List.Item>
+            )}
+          />
+        </Modal>
+        {/* Settings Modal for Logout */}
+      <Modal
+        title="Settings"
+        visible={isSettingsVisible}
+        onCancel={handleCancelSettings}
         footer={null}
       >
-        <List
-          itemLayout="horizontal"
-          dataSource={users}
-          renderItem={(user) => (
-            <List.Item onClick={() => handleLogin(user._id) } className="user-item">
-              <List.Item.Meta
-                title={user.first_name + ' ' + user.last_name}  
-                description={user.email}
-              />
-            </List.Item>
-          )}
-        />
+        <Button
+          type="primary" danger onClick={handleLogout} block className="logout-btn"  
+          >Logout
+        </Button>
       </Modal>
     </Layout>
   );
--- libary-management-system/src/Context/UserContext.js
+++ libary-management-system/src/Context/UserContext.js
@@ -4,9 +4,10 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userId, setUserId] = useState(null);
+  const [userName, setUserName] = useState(null); // Add userName state
 
   return (
-    <UserContext.Provider value={{ userId, setUserId }}>
+    <UserContext.Provider value={{ userId, setUserId, userName, setUserName }}>
       {children}
     </UserContext.Provider>
   );
