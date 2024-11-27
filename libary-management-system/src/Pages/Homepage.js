58d1b5fa92a8d2c0a5368105e3ccd8c88168c69c
shubz0 <sohaib.khan.khan11@gmail.com>
2024-11-27 15:06:41 +0000

    Carousel
    Added images to the carousel on the homepage, pushed the carousel to the top of the home page, enabled auto scroll, added an opaque text box over the carousel for hovering effect and to add some information to guide user when they first see the home page

--- libary-management-system/src/Pages/Homepage.js
+++ libary-management-system/src/Pages/Homepage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Carousel, Button, Flex, Splitter, Typography } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import '../CSS/homepage.css';
 
 //Assets
 import logo1 from '../Assets/logo1.png';
 import logo2 from '../Assets/logo2.png';
 import logo3 from '../Assets/logo3.png';
+import slide1 from '../Assets/slide1.jpg';
+import slide2 from '../Assets/slide2.jpg';
+import slide3 from '../Assets/slide3.jpg';
+import slide4 from '../Assets/slide4.jpg';
 
 
 const blueBoxStyle = {
-  height: '50vh',
-  backgroundColor: '#FFFFFF',
+  height: '60vh',
+  backgroundColor: '#F0F5F5',
   color: '#000',
   display: 'flex',
   flexDirection: 'column',
@@ -23,13 +28,9 @@ const blueBoxStyle = {
   fontSize: '24px',
 };
 
-const carouselContainerStyle = {
-  height: '40vh',
-};
-
 const contentStyle = {
   margin: 0,
-  height: '370px',
+  height: '100%',
   color: '#fff',
   lineHeight: '160px',
   textAlign: 'center',
@@ -57,29 +58,49 @@ const Homepage = () => {
 
   return (
     <div style={{display: 'flex', flexDirection: 'column' }}>
+
+    {/* Carousel Container */}
+    <div className='carousel-container'>
+    <Carousel autoplay autoplaySpeed={3000} arrows infinite>
+      <div>
+        <img src={slide1} alt='Slide 1' className='carousel-image' />
+      </div>
+      <div>
+      <img src={slide2} alt='Slide 2' className='carousel-image' />
+      </div>
+      <div>
+      <img
+        src={slide3} alt='Slide 3' className='carousel-image' />
+      </div>
+      <div>
+      <img src={slide4} alt='Slide 4' className='carousel-image' />
+      </div>
+    </Carousel>
+
+    {/* Carousel Hovering Box */}
+    <div className='carousel-text-box'>
+      <h2>Explore the World of knowledge</h2>
+      <p>
+        Discover books, movies, and games to enlighten your mind and inspire your imagination
+      </p>
+      <Button type='primary' onClick={handleGetStarted}>
+        Lets Go!
+      </Button>
+    </div>
+    </div>
+
+
+
+
+
+    
       <div style={blueBoxStyle}>
         <h1 className="heading">Welcome to AML - Your Gateway to the UK's Largest Library Network</h1>
         <p>Discover a world of knowledge with the Advanced Media Library (AML), offering books, journals, multimedia, and more across England's largest library network. Whether you're in Sheffield, Manchester, or a smaller town, AML provides seamless access to resources through our in-branch services, web-mobile app, and 24/7 online systems.</p>
         <Button type="primary" onClick={handleGetStarted}>Get Started!</Button>
       </div>
 
-      {/* Carousel Container */}
-      <div style={carouselContainerStyle}>
-        <Carousel arrows infinite={false}>
-          <div>
-            <h3 style={contentStyle}>1</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>2</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>3</h3>
-          </div>
-          <div>
-            <h3 style={contentStyle}>4</h3>
-          </div>
-        </Carousel>
-      </div>
+      
 
      
       <div style={{ textAlign: 'center', marginTop: '30px' }}>
